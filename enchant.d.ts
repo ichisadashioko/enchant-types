9fdf5a0bcd8439dc1056a9aac715f6fb62c7f7a5
Ichisada Shioko <ichisadashioko@gmail.com>
2019-11-12 10:47:28 +0700

    :labal:

---
+++ enchant.d.ts
@@ -0,0 +1,5 @@
+// Type definitions for enchant.js 0.8.6
+// Project: https://github.com/wise9/enchant.js
+// Definitions by: shioko <https://github.com/ichisadashioko>
+
+declare function enchant(): void;
